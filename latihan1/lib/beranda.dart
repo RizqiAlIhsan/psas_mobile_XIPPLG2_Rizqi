95ae28c307d78d02e6ff4ed5bc04dc342dd7348d
RizqiAlIhsan <rizqimarhas@gmail.com>
2024-11-19 09:34:18 +0700

    commit pertama

---
+++ latihan1/lib/beranda.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class beranda extends StatelessWidget {
+  const beranda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        child: Text("Hello world"),
+      )
+    );
+  }
+}
